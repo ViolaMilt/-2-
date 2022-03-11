6078b56785c3a3c09590ca22acce8bc65e7870c2
Viola <viola-doc@yandex.ru>
2022-03-12 00:42:16 +0300

    Задача 10.Второй вариант

--- Домашнее задание 2/Program.cs
+++ Домашнее задание 2/Program.cs
@@ -9,3 +9,11 @@
             }
             Console.WriteLine(arr[2] +""+arr[1] +""+arr[0]);
 
+
+//Задача 10.Второй вариант  
+  Console.WriteLine ( "Введите техзначное число:  ");
+ string n = Console.ReadLine();
+ string itog = string.Concat(k[2] , k[1] , k[0]);
+Console.WriteLine(itog);
+
+
