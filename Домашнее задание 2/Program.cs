88cdd30130dcfa756a1721f951274df6ce10abd0
Viola <viola-doc@yandex.ru>
2022-03-12 00:38:57 +0300

    Задача 10

---
+++ Домашнее задание 2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 10
+ Console.WriteLine ( "Введите трехзначное число:  ");
+ string k = Console.ReadLine();
+ 
+   char[] arr = new char[k.Length];
+            for (int i = 0; i < k.Length; i++)
+            {
+               arr[i] = k[i];
+            }
+            Console.WriteLine(arr[2] +""+arr[1] +""+arr[0]);
+
